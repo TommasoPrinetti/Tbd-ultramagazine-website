ed4ba025aabf236f1278411d98122a0d91cfc6b3
TommasoPrinetti <Tommi.prinetti@gmail.com>
2025-11-02 17:52:32 +0100

    Addedvercel pipeline

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DyQg9SqJ.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/q-bSWnWw.js","_app/immutable/chunks/Ga0CjPpz.js","_app/immutable/chunks/DoV-FpeZ.js","_app/immutable/chunks/Dy4U-Nbq.js","_app/immutable/chunks/DJwrhxke.js","_app/immutable/chunks/ZQvcI8pd.js"];
+export const imports = ["_app/immutable/nodes/1.CtJmhHdb.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/q-bSWnWw.js","_app/immutable/chunks/Ga0CjPpz.js","_app/immutable/chunks/BhZYN2S9.js","_app/immutable/chunks/Dy4U-Nbq.js","_app/immutable/chunks/DJwrhxke.js","_app/immutable/chunks/D9FocfZ9.js"];
 export const stylesheets = ["_app/immutable/assets/header.BuuigBQi.css","_app/immutable/assets/1.4XzbPFNL.css"];
 export const fonts = [];
